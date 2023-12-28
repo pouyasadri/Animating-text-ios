386df2db6d27c085b25c94adf5b46f5c9684c018
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-28 16:28:34 +0100

    Initial Commit

---
+++ AnimatingText/AnimatingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingTextApp.swift
+//  AnimatingText
+//
+//  Created by Pouya Sadri on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
